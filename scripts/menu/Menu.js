efa3519436edbc4d56f320d84a3ee26cb5a6975a
vanoode <vanoode@gmail.com>
2013-06-28 00:47:30 +0200

    komit may czendżes

--- scripts/menu/Menu.js
+++ scripts/menu/Menu.js
@@ -2,34 +2,34 @@ var myScroll = false;
 var Menu = {
     init: function() {
         $(document).ready(function() {
-            $('#slideMenu').sidr();
-            $('#slideMenu').unbind('click').click(function() {
-                Menu.toggle();
-            });
-            $('#menuOverlay').click(function() {
-                Menu.toggle();
-            });
-            $(document).wipetouch({
-                tapToClick: true,
-                wipeLeft: function(result) {
-                    $.sidr('close', 'sidr', function() {
-                        $('#menuOverlay').css('display', 'none');
-                    });
-                },
-                wipeRight: function(result) {
-                    $('#menuOverlay').css('display', 'block');
-                    $.sidr('open', 'sidr', function() {
-                        if (!myScroll) {
-                            myScroll = new iScroll('sidr');
-                        }
-                    });
-                }
-            });
-//            $('#menuBtn').attr('onclick', 'alert("dupa")');
-////            console.log();
-//            $('#menuBtn').click(function() {
-//                alert('test');
-////                
+            
+            $('#obrazek').load(function() {
+                $(this).addClass('animate');
+                console.log('zaladowany');
+            })
+//            $('#slideMenu').sidr();
+//            $('#slideMenu').unbind('click').click(function() {
+//                Menu.toggle();
+//            });
+//            $('#menuOverlay').click(function() {
+//                Menu.toggle();
+//            });
+//            $(window).wipetouch({
+//                preventDefault: false, 
+//                tapToClick: false,
+//                wipeLeft: function(result) {
+//                    $.sidr('close', 'sidr', function() {
+//                        $('#menuOverlay').css('display', 'none');
+//                    });
+//                },
+//                wipeRight: function(result) {
+//                    $('#menuOverlay').css('display', 'block');
+//                    $.sidr('open', 'sidr', function() {
+//                        if (!myScroll) {
+//                            myScroll = new iScroll('sidr');
+//                        }
+//                    });
+//                }
 //            });
         });
     },
