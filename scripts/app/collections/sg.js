c1d52630f1cd08b0d8ce54a178a1c35a46dbc905
gibux <ksi.marcin@gmail.com>
2013-06-27 13:45:03 +0200

    Sg more

--- scripts/app/collections/sg.js
+++ scripts/app/collections/sg.js
@@ -1,11 +1,20 @@
-define(['app/models/sgdetail'],function(SgDetail){
+define(['app/models/sgdetail','app/views/sgdetail'],function(SgDetail,SgDetailView){
 	var Sg = Backbone.Collection.extend({
 		model: SgDetail,
 		fetch:  function(){
 			var that = this;
 			queryOnet.getAllNews({}, function(err, result){
-
-			};
+				if(result){
+					console.log(result);
+					that.add(result);
+					that.render();
+				}
+			});
+		},
+		render: function(){
+			this.each(function(element){
+				var view = new SgDetailView({model: element});
+			});
 		}
 	});
 	return Sg;
--- scripts/app/models/sgdetail.js
+++ scripts/app/models/sgdetail.js
@@ -1,6 +1,9 @@
-define([], function(){
+define(['app/views/sgdetail'], function(SgDetailView){
 	var SgDetail = Backbone.Model.extend({
 		
+		render: function(){
+			console.log(render);
+		}
 	});
 
 	return SgDetail;
