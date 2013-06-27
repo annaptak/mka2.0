530611deb8c922d991ddcff6f5194071de769f47
vanoode <vanoode@gmail.com>
2013-06-27 21:16:49 +0200

    Menu na SG

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,17 @@
 requirejs.config({
 	  paths: {
-	    jQuery: '/scripts/jquery-2.0.2.min',
+	    jQuery: 'jquery',
+	    sidr: '/scripts/menu/jquery.sidr.min',
+	    wipetouch: '/scripts/menu/jquery.wipetouch',
+	    iscroll: '/scripts/iscroll',
+	    Menu: '/scripts/menu/Menu',
 	    Underscore: '/scripts/underscore',
 	    Backbone: '/scripts//backbone',
 	    JSONPLoader: '/scripts/JSONPLoader',
 	    queryOnet: '/scripts/queryOnet'
 	  },	
 		shim: {
-			'Backbone': ['Underscore', 'jQuery','JSONPLoader','queryOnet'],
+			'Backbone': ['Underscore', 'jQuery','sidr','wipetouch','iscroll','Menu','JSONPLoader','queryOnet'],
 			'm': ['Backbone']
 		}
 });
