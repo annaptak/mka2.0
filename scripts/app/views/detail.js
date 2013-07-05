f545ca0d62495234cd9e2daea9689fe4ec396936
Marko Cilimkovic <os.chilim@gmail.com>
2013-07-05 15:05:18 +0200

    Creates event for swiping back from article page to home page
    Swipe right and you're back to index page :)

--- scripts/app/views/detail.js
+++ scripts/app/views/detail.js
@@ -1,21 +1,22 @@
 define([],function(){
 	var Detail = Backbone.View.extend({
-		tagName: 'div',
-		events:{
-			'touchstart #sg': 'onScroll',
-			'swipe' : 'backHome'
-		},
+		
 		template: _.template($('#detail').html()),
+		
 		initialize: function(){
 			//this.listenTo(this.model, "change", this.render);
 			//this.listenTo(this.model, "destroy", this.remove);
 			console.log('Detail View Init');
             $('#loader').remove();
+			$('#wrapper').live('swipeRight', function(event) {
+				window.history.back();
+				return false;
+			});	
 			this.render();
 		},
+		
 		render: function(){
 			console.log(this.el);
-			console.log('*************************');
 			console.log(this.model.toJSON()['news']['meta']['identifier'], window.ids);
 			if(window.backVar){
 				$('body').slideFrom(this.template(this.model.toJSON()));
@@ -27,15 +28,7 @@ define([],function(){
 			this.$el.html(this.template(this.model.toJSON()));
 			window.ids.push(this.model.toJSON()['news']['meta']['identifier']);
 			
-			//$('#wrapper').append(this.$el);
-		},
-		
-		backHome: function() {
-			console.log("Swiping in detailview");
-		},
-		
-		onScroll: function(){
-			console.log("click");
+			$('#wrapper').append(this.$el);
 		},
 		
 	});
